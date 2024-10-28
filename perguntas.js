f1b5ef6a10a8628ad3a5aed07bca49476e5f5a9d
Cristian Eduardo Soares <137203664+CristianEduardoSoares@users.noreply.github.com>
2024-10-28 08:20:36 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Química',
+    'O que são os hidrocarbonetos?',
+    'São um grupo de substâncias orgânicas que possuem apenas carbono e hidrogênio em sua estrutura'
+)
+
+criaCartao(
+    'Química',
+    'O que são anéis aromáticos?',
+    'São estruturas cíclicas de hidrocarbonetos com ligações duplas alternadas'
+)
+
+criaCartao(
+    'Química',
+    'Qual o critério utilizado para classificar carbonos?',
+    'Os carbonos são classificados com base no número de carbonos vizinhos'
+)
+
+criaCartao(
+    'Química',
+    'Como um carbono pode ser classificado?',
+    'Pode ser classificado em primário, secundário, terciário ou quaternário'
+)
+
+criaCartao(
+    'Química',
+    'Como uma cadeia carbônica pode ser classificada?',
+    'Pode ser classificada em fechada, aberta ou mista, saturada ou insaturada, homogênea ou heterogênea e normal ou ramificada'
+)
+
+criaCartao(
+    'Química',
+    'Qual a fórmula molecular do benzeno?',
+    'A fórmula molecular do benzeno é C₆H₆'
+)
+
+criaCartao(
+    'Química',
+    'Por que o carbono é considerado um elemento tetravalente?',
+    'Porque precisa de 4 ligações para ficar completo'
+)
+
+criaCartao(
+    'Química',
+    'O que cada extremidade representa na fórmula de bastão?',
+    'Cada extremidade representa um átomo de carbono'
+)
+
+criaCartao(
+    'Química',
+    'O que é a química orgânica?',
+    'É uma área da química que estuda os compostos formados predominantemente por carbono e hidrogênio e suas estruturas, propriedades e reatividades. '
+)
